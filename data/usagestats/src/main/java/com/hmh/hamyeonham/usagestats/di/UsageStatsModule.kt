bf25b017983994cd16cb28cadbd87b7bc6a6588d
jihyun0v0 <jihyuneee01@gmail.com>
2024-06-13 22:17:22 +0900

    [fix]: datastore -> datasource로 패키지 refact

--- data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/di/UsageStatsModule.kt
+++ data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/di/UsageStatsModule.kt
@@ -14,7 +14,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import datastore.DefaultHMHDeletedAppUsagePreference
+import com.hmh.hamyeonham.usagestats.datasource.local.DefaultHMHDeletedAppUsagePreference
 import com.hmh.hamyeonham.usagestats.datastore.HMHDeletedAppUsagePreference
 import javax.inject.Singleton
 
