e9e1f36ed566f849d5f86e1fff047b99c513e0a3
jihyun0v0 <jihyuneee01@gmail.com>
2024-06-14 02:15:38 +0900

    [feat]: 앱삭제용 repository 추가

--- data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/di/UsageStatsModule.kt
+++ data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/di/UsageStatsModule.kt
@@ -3,10 +3,14 @@ package com.hmh.hamyeonham.usagestats.di
 import android.app.usage.UsageStatsManager
 import android.content.Context
 import com.hmh.hamyeonham.core.domain.usagegoal.repository.UsageGoalsRepository
+import com.hmh.hamyeonham.usagestats.datasource.local.DeletedAppUsageLocalDataSource
+import com.hmh.hamyeonham.usagestats.datasource.local.DeletedAppUsageLocalDataSourceImpl
 import com.hmh.hamyeonham.usagestats.datasource.local.UsageStatusLocalDataSource
 import com.hmh.hamyeonham.usagestats.datasource.local.UsageStatusLocalDataSourceImpl
+import com.hmh.hamyeonham.usagestats.repository.DefaultDeleteGoalRepository
 import com.hmh.hamyeonham.usagestats.repository.DefaultUsageGoalsRepository
 import com.hmh.hamyeonham.usagestats.repository.DefaultUsageStatsRepository
+import com.hmh.hamyeonham.usagestats.repository.DeleteGoalRepository
 import com.hmh.hamyeonham.usagestats.repository.UsageStatsRepository
 import dagger.Binds
 import dagger.Module
@@ -14,8 +18,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import com.hmh.hamyeonham.usagestats.datasource.local.DefaultHMHDeletedAppUsagePreference
-import com.hmh.hamyeonham.usagestats.datastore.HMHDeletedAppUsagePreference
 import javax.inject.Singleton
 
 @Module
@@ -37,14 +39,19 @@ object UsageStatsModule {
 
         @Binds
         @Singleton
+        fun provideDeletedAppUsageDataSource(deletedAppUsageLocalDataSource: DeletedAppUsageLocalDataSourceImpl): DeletedAppUsageLocalDataSource
+
+        @Binds
+        @Singleton
         fun provideUsageStatsRepository(usageStatsRepository: DefaultUsageStatsRepository): UsageStatsRepository
 
         @Binds
         @Singleton
         fun provideUsageGoalsRepository(usageGoalsRepository: DefaultUsageGoalsRepository): UsageGoalsRepository
 
-        @Singleton
         @Binds
-        fun bindAppPreferences(dataStore: DefaultHMHDeletedAppUsagePreference): HMHDeletedAppUsagePreference
+        @Singleton
+        fun providesDeleteGoalsRepository(deleteGoalRepository: DefaultDeleteGoalRepository): DeleteGoalRepository
+
     }
 }
