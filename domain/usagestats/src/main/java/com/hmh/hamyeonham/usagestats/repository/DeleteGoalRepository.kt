f3516451b48be725a1d78f119d36d4b1b6511929
jihyun0v0 <jihyuneee01@gmail.com>
2024-06-14 14:52:52 +0900

    [refact]: 리팩토링

--- domain/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/repository/DeleteGoalRepository.kt
+++ domain/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/repository/DeleteGoalRepository.kt
@@ -3,8 +3,6 @@ package com.hmh.hamyeonham.usagestats.repository
 interface DeleteGoalRepository {
     suspend fun getDeletedUsage(date: String): Long
     suspend fun getDeletedUsageOfToday(): Long
-
     suspend fun addDeletedGoal(packageName: String, usage: Long)
-
     suspend fun checkAndRevertDeletedGoal(packageName: String)
 }
