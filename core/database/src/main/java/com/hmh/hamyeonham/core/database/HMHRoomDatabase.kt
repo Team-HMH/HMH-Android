30026126632269f2872a12c6486d8273e6be5a88
jihyun0v0 <jihyuneee01@gmail.com>
2024-06-14 12:22:46 +0900

    [delete]: db migration 내용 삭제

--- core/database/src/main/java/com/hmh/hamyeonham/core/database/HMHRoomDatabase.kt
+++ core/database/src/main/java/com/hmh/hamyeonham/core/database/HMHRoomDatabase.kt
@@ -2,8 +2,6 @@ package com.hmh.hamyeonham.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.hmh.hamyeonham.core.database.dao.ChallengeDao
 import com.hmh.hamyeonham.core.database.dao.DeletedGoalsDao
 import com.hmh.hamyeonham.core.database.dao.UsageGoalsDao
@@ -27,7 +25,7 @@ import kotlinx.coroutines.launch
         DeletedGoalWithUsageEntity::class,
         DeletedUsageEntity::class
     ],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 abstract class HMHRoomDatabase : RoomDatabase() {
@@ -43,26 +41,6 @@ abstract class HMHRoomDatabase : RoomDatabase() {
             usageTotalGoalDao().deleteAll()
             challengeDao().deleteAll()
             deletedGoalsDao().deleteAll()
-        }
-
-    }
-
-    companion object {
-        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                // deleted_goal_with_usages 테이블 생성
-                database.execSQL(
-                    "CREATE TABLE IF NOT EXISTS `deleted_goal_with_usages` (`date` TEXT NOT NULL, `totalUsage` INTEGER NOT NULL, PRIMARY KEY(`date`), FOREIGN KEY(`date`) REFERENCES `deleted_usage`(`date`) ON DELETE CASCADE)"
-                )
-                // deleted_usage 테이블 생성
-                database.execSQL(
-                    "CREATE TABLE IF NOT EXISTS `deleted_usage` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `packageName` TEXT NOT NULL, `usage` INTEGER NOT NULL, `date` TEXT NOT NULL)"
-                )
-                // deleted_usage 테이블에 date 열에 고유 인덱스 추가
-                database.execSQL(
-                    "CREATE UNIQUE INDEX IF NOT EXISTS `index_deleted_usage_date` ON `deleted_usage` (`date`)"
-                )
-            }
         }
 
     }
