cd30db9d051ec7cb3c29d3a02b1a84140c7fff88
KwakEuiJin <yuijin0909@gmail.com>
2024-06-15 11:52:49 +0900

    [fix]: UseCase 함수 suspend로 변경

--- domain/lock/src/main/java/com/hmh/hamyeonham/lock/SetIsUnLockUseCase.kt
+++ domain/lock/src/main/java/com/hmh/hamyeonham/lock/SetIsUnLockUseCase.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 class SetIsUnLockUseCase @Inject constructor(
     private val repository: LockRepository
 ){
-    operator fun invoke(isUnLock: Boolean){
+    suspend operator fun invoke(isUnLock: Boolean){
         repository.setIsUnLock(isUnLock)
     }
 }
