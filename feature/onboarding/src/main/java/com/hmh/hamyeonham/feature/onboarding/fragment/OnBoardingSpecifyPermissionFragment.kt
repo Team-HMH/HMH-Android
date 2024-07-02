23dfc18442eb3a47c5faf1afb2436ca245acf25d
kangyuri1114 <yuri3334@gmail.com>
2024-07-02 17:52:39 +0900

    [feat]: 접근성 permission 로직 이동

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSpecifyPermissionFragment.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSpecifyPermissionFragment.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.onboarding.R
 import com.hmh.hamyeonham.feature.onboarding.databinding.FragmentOnboardingSpecifyPermissionBinding
 import com.hmh.hamyeonham.feature.onboarding.viewmodel.OnBoardingViewModel
 import com.hmh.hamyeonham.feature.onboarding.viewmodel.OnboardEvent
 
 class OnBoardingSpecifyPermissionFragment : Fragment() {
+    private val binding by viewBinding(FragmentOnboardingSpecifyPermissionBinding::bind)
     private val activityViewModel by activityViewModels<OnBoardingViewModel>()
 
     override fun onCreateView(
@@ -19,6 +21,27 @@ class OnBoardingSpecifyPermissionFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View = FragmentOnboardingSpecifyPermissionBinding.inflate(inflater, container, false).root
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val text = binding.tvOnboardingSpecifyPermissionList
+//        val spanText = SpannableStringBuilder(text.text)
+//        spanText.setSpan(
+//            TextAppearanceSpan(text.context, com.hmh.hamyeonham.core.designsystem.R.style.TextAppearance_Hmh_LabelLarge),
+//            0,
+//            39,
+//            SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE
+//        )
+//        text.text = spanText
+
+        binding.tvPermissionLater.setOnClickListener {
+            activityViewModel.updateState { copy(navigateToPermissionView = true) }
+        }
+    }
 
     override fun onResume() {
         super.onResume()
