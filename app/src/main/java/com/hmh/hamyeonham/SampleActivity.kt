289c9935cb3969b964c591976cc941acf89f9843
kangyuri1114 <yuri3334@gmail.com>
2024-07-07 20:42:08 +0900

    [feat]: 주석 제거

--- app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
@@ -7,7 +7,6 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.databinding.ActivitySampleBinding
 import com.hmh.hamyeonham.feature.login.LoginActivity
-import com.hmh.hamyeonham.feature.onboarding.OnBoardingActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -30,9 +29,9 @@ class SampleActivity : AppCompatActivity() {
     private fun initLottieSplash() {
         binding.splashLottieAppLogo.playAnimation()
 
-        coroutineScope.launch{
+        coroutineScope.launch {
             delay(4000)
-            navigateToLogin ()
+            navigateToLogin()
         }
     }
 
