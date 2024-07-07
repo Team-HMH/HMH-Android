ab42f76379b46d31456871683898287f71214455
kangyuri1114 <yuri3334@gmail.com>
2024-07-07 18:56:04 +0900

    [feat]: add PermissionDescriptionActivity in Home

--- feature/main/src/main/java/com/hmh/hamyeonham/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/hmh/hamyeonham/feature/main/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.hmh.hamyeonham.feature.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -9,16 +8,13 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.hmh.hamyeonham.challenge.worker.ChallengeDateSaveWorker
-import com.hmh.hamyeonham.common.activity.allPermissionIsGranted
 import com.hmh.hamyeonham.common.activity.isBatteryOptimizationEnabled
 import com.hmh.hamyeonham.common.activity.requestDisableBatteryOptimization
 import com.hmh.hamyeonham.common.context.getAppNameFromPackageName
 import com.hmh.hamyeonham.common.dialog.OneButtonCommonDialog
 import com.hmh.hamyeonham.common.dialog.TwoButtonCommonDialog
 import com.hmh.hamyeonham.common.navigation.NavigationProvider
-import com.hmh.hamyeonham.common.permission.PermissionActivity
 import com.hmh.hamyeonham.common.view.viewBinding
-import com.hmh.hamyeonham.core.service.lockAccessibilityServiceClassName
 import com.hmh.hamyeonham.core.viewmodel.MainEffect
 import com.hmh.hamyeonham.core.viewmodel.MainViewModel
 import com.hmh.hamyeonham.feature.main.databinding.ActivityMainBinding
