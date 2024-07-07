f5b805cdb67a53b8a18219db083d9cf5833203a1
kangyuri1114 <yuri3334@gmail.com>
2024-07-07 23:12:47 +0900

    [feat]: 접근성 허용 분리

--- core/common/src/main/java/com/hmh/hamyeonham/common/permission/PermissionDescriptionActivity.kt
+++ core/common/src/main/java/com/hmh/hamyeonham/common/permission/PermissionDescriptionActivity.kt
@@ -2,14 +2,30 @@ package com.hmh.hamyeonham.common.permission
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.hmh.hamyeonham.common.activity.checkAccessibilityServiceEnabled
+import com.hmh.hamyeonham.common.context.toast
 import com.hmh.hamyeonham.common.databinding.ActivityPermissionDescriptionBinding
+import com.hmh.hamyeonham.common.navigation.NavigationProvider
 import com.hmh.hamyeonham.common.view.viewBinding
+import com.hmh.hamyeonham.core.designsystem.R
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class PermissionDescriptionActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityPermissionDescriptionBinding::inflate)
+
+    @Inject
+    lateinit var navigationProvider: NavigationProvider
+
+    override fun onResume() {
+        super.onResume()
+        checkAccessibilityAndNavigateToPermission()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +36,37 @@ class PermissionDescriptionActivity : AppCompatActivity() {
 
     private fun setButtonClickListener() {
         binding.btnNext.setOnClickListener {
-            val intent = Intent(this, PermissionActivity::class.java)
-            startActivity(
-                intent
-                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                    .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            )
-            finish()
+            requestAccessibilitySettings()
         }
         binding.ivBack.setOnClickListener { finish() }
+    }
+
+    private val accessibilitySettingsLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+        ) {
+            checkAccessibilityAndNavigateToPermission()
+        }
+
+    private fun checkAccessibilityAndNavigateToPermission() {
+        if (checkAccessibilityServiceEnabled()) {
+            toast(getString(R.string.success_accessibility_settings))
+            startActivity(navigationProvider.toPermission())
+            finish()
+        }
+    }
+
+    private fun requestAccessibilitySettings() {
+        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+        accessibilitySettingsLauncher.launch(intent)
+    }
+
+    private fun checkAccessibilityServiceEnabled(): Boolean {
+        val service = "$packageName/com.hmh.hamyeonham.core.service.LockAccessibilityService"
+        val enabledServicesSetting = Settings.Secure.getString(
+            contentResolver,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
+        )
+        return enabledServicesSetting?.contains(service) == true
     }
 }
