c99d733023f73178789cb5cc9825e456dd113630
kangyuri1114 <yuri3334@gmail.com>
2024-08-18 02:18:12 +0900

    [feat]: amplitude configureAndroidCommonPlugin로 이동

---
+++ core/common/src/main/java/com/hmh/hamyeonham/common/amplitude/AmplitudeUtils.kt
@@ -0,0 +1,19 @@
+package com.hmh.hamyeonham.common.amplitude
+
+import com.amplitude.api.Amplitude
+import org.json.JSONObject
+
+object AmplitudeUtils {
+    private val amplitude = Amplitude.getInstance()
+
+    fun trackEventWithProperties(
+        eventName: String,
+        properties: JSONObject? = null,
+    ) {
+        if (properties == null) {
+            amplitude.logEvent(eventName)
+        } else {
+            amplitude.logEvent(eventName, properties)
+        }
+    }
+}
