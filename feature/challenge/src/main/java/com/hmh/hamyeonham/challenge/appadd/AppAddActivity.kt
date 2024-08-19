a6240e4322614e15eec34517ae1b9d5d623cc655
kangyuri1114 <yuri3334@gmail.com>
2024-08-20 03:18:15 +0900

    [feat]: amplitude "view_add_totaltime" 이벤트 추가

--- feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/AppAddActivity.kt
+++ feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/AppAddActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.hmh.hamyeonham.common.amplitude.AmplitudeUtils
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.challenge.databinding.ActivityAppAddBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,7 +16,6 @@ import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class AppAddActivity : AppCompatActivity() {
-
     companion object {
         const val SELECTED_APPS = "selected_apps"
         const val GOAL_TIME = "goal_time"
@@ -23,6 +23,11 @@ class AppAddActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityAppAddBinding::inflate)
     private val viewModel by viewModels<AppAddViewModel>()
+
+    override fun onResume() {
+        super.onResume()
+        AmplitudeUtils.trackEventWithProperties("view_add_totaltime")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,33 +45,33 @@ class AppAddActivity : AppCompatActivity() {
             btAppSelection.setOnClickListener { handleNextClicked() }
             ivBack.setOnClickListener { finish() }
         }
-
     }
 
     private fun collectState() {
-        viewModel.isNextButtonActive.flowWithLifecycle(lifecycle)
+        viewModel.isNextButtonActive
+            .flowWithLifecycle(lifecycle)
             .onEach { binding.btAppSelection.isEnabled = it }
             .launchIn(lifecycleScope)
     }
 
-
     private fun collectEffect() {
-        viewModel.effect.flowWithLifecycle(lifecycle).onEach {
-            when (it) {
-                is AppAddEffect.ShowLoading -> {
-                    binding.pbLoading.isVisible = true
-                }
+        viewModel.effect
+            .flowWithLifecycle(lifecycle)
+            .onEach {
+                when (it) {
+                    is AppAddEffect.ShowLoading -> {
+                        binding.pbLoading.isVisible = true
+                    }
 
-                is AppAddEffect.HideLoading -> {
-                    binding.pbLoading.isVisible = false
-                }
+                    is AppAddEffect.HideLoading -> {
+                        binding.pbLoading.isVisible = false
+                    }
 
-                is AppAddEffect.NONE -> {
+                    is AppAddEffect.NONE -> {
+                    }
                 }
-            }
-        }.launchIn(lifecycleScope)
+            }.launchIn(lifecycleScope)
     }
-
 
     private fun handleNextClicked() {
         binding.vpAppAdd.run {
@@ -78,10 +83,15 @@ class AppAddActivity : AppCompatActivity() {
     }
 
     private fun finishWithResults() {
-        val intent = Intent().apply {
-            putExtra(SELECTED_APPS, viewModel.state.value.selectedApps.toTypedArray())
-            putExtra(GOAL_TIME, viewModel.state.value.goalTime)
-        }
+        val intent =
+            Intent().apply {
+                putExtra(
+                    SELECTED_APPS,
+                    viewModel.state.value.selectedApps
+                        .toTypedArray(),
+                )
+                putExtra(GOAL_TIME, viewModel.state.value.goalTime)
+            }
         setResult(RESULT_OK, intent)
         finish()
     }
