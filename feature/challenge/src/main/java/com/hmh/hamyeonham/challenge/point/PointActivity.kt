cc1224169ecad9e6caf0edcef053333aeeb82324
kangyuri1114 <yuri3334@gmail.com>
2024-08-19 17:09:11 +0900

    [feat]: amplitude "view_point" 이벤트 추가

--- feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/point/PointActivity.kt
+++ feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/point/PointActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.hmh.hamyeonham.common.amplitude.AmplitudeUtils
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.challenge.databinding.ActivityPointBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,11 @@ import kotlinx.coroutines.flow.onEach
 class PointActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityPointBinding::inflate)
     private val viewModel by viewModels<PointViewModel>()
+
+    override fun onResume() {
+        super.onResume()
+        AmplitudeUtils.trackEventWithProperties("view_point")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +37,12 @@ class PointActivity : AppCompatActivity() {
     }
 
     private fun initAdapter() {
-        val adapter = PointAdapter(
-            onButtonClick = { challengeDate ->
-                viewModel.earnChallengePoint(challengeDate)
-            },
-        )
+        val adapter =
+            PointAdapter(
+                onButtonClick = { challengeDate ->
+                    viewModel.earnChallengePoint(challengeDate)
+                },
+            )
         binding.rvPoint.run {
             this.adapter = adapter
             layoutManager = LinearLayoutManager(context)
@@ -49,14 +56,16 @@ class PointActivity : AppCompatActivity() {
     }
 
     private fun collectPointInfo() {
-        viewModel.pointInfoList.flowWithLifecycle(lifecycle)
+        viewModel.pointInfoList
+            .flowWithLifecycle(lifecycle)
             .onEach { pointInfoList ->
                 (binding.rvPoint.adapter as? PointAdapter)?.submitList(pointInfoList)
             }.launchIn(lifecycleScope)
     }
 
     private fun collectUserPoint() {
-        viewModel.currentPointState.flowWithLifecycle(lifecycle)
+        viewModel.currentPointState
+            .flowWithLifecycle(lifecycle)
             .onEach {
                 binding.tvPointTotal.text = it.toString()
                 setResult(RESULT_OK, intent.putExtra("point", it))
