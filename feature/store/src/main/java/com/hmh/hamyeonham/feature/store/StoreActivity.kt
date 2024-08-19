b6a3576cfba0b0fd30dd5fad8657b545eab147a8
kangyuri1114 <yuri3334@gmail.com>
2024-08-19 18:50:41 +0900

    [feat]: amplitude "view_shop" 이벤트 추가

--- feature/store/src/main/java/com/hmh/hamyeonham/feature/store/StoreActivity.kt
+++ feature/store/src/main/java/com/hmh/hamyeonham/feature/store/StoreActivity.kt
@@ -1,13 +1,18 @@
 package com.hmh.hamyeonham.feature.store
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hmh.hamyeonham.common.amplitude.AmplitudeUtils
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.store.databinding.ActivityStoreBinding
 
 class StoreActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityStoreBinding::inflate)
+
+    override fun onResume() {
+        super.onResume()
+        AmplitudeUtils.trackEventWithProperties("view_shop")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
