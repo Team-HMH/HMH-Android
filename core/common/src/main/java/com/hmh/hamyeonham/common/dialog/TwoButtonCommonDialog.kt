05b8d02148cc20f493cbb1aa9bf6d1142ed35713
jihyun0v0 <jihyuneee01@gmail.com>
2024-08-23 00:28:18 +0900

    [fix]: dialog xml수정, two button img = null인 경우 수정

--- core/common/src/main/java/com/hmh/hamyeonham/common/dialog/TwoButtonCommonDialog.kt
+++ core/common/src/main/java/com/hmh/hamyeonham/common/dialog/TwoButtonCommonDialog.kt
@@ -55,15 +55,20 @@ class TwoButtonCommonDialog : DialogFragment() {
     private fun initViews() {
         val title = arguments?.getString(TITLE, "")
         val description = arguments?.getString(DESCRIPTION)
-        val iconRes = arguments?.getInt(OneButtonCommonDialog.ICON_RES)
+        val iconRes = arguments?.getInt(OneButtonCommonDialog.ICON_RES, 0)
         val confirmButtonText = arguments?.getString(CONFIRM_BUTTON_TEXT, "")
         val dismissButtonText = arguments?.getString(DISMISS_BUTTON_TEXT, "")
+        val hideIcon = arguments?.getBoolean(HIDE_ICON) ?: false
 
         with(binding) {
             tvDialogTitle.text = title
             tvDialogDescription.text = description
-            iconRes?.let { ivDialogIcon.setImageResource(it) } ?: run {
+            if (hideIcon)
                 ivDialogIcon.isGone = true
+            else {
+                iconRes?.let {
+                    ivDialogIcon.setImageResource(it)
+                } ?: run { ivDialogIcon.isGone = true }
             }
             tvConfirmButton.text = confirmButtonText
             tvDismissButton.text = dismissButtonText
@@ -93,6 +98,8 @@ class TwoButtonCommonDialog : DialogFragment() {
         const val DESCRIPTION = "description"
         const val CONFIRM_BUTTON_TEXT = "confirmButtonText"
         const val DISMISS_BUTTON_TEXT = "dismissButtonText"
+        const val ICON_RES = "iconRes"
+        const val HIDE_ICON = "hideIcon"
 
 
         fun newInstance(
@@ -107,7 +114,7 @@ class TwoButtonCommonDialog : DialogFragment() {
                     putString(TITLE, title)
                     putString(DESCRIPTION, description)
                     putString(CONFIRM_BUTTON_TEXT, confirmButtonText)
-                    iconRes?.let { putInt(OneButtonCommonDialog.ICON_RES, it) }
+                    iconRes?.let { putInt(ICON_RES, it) } ?: run { putBoolean(HIDE_ICON, true) }
                     putString(DISMISS_BUTTON_TEXT, dismissButtonText)
                 }
             }
