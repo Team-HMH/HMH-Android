d2eaccb17c3d634dbc92dd47c2737a9cce2dc39a
Kez <yuijin0909@gmail.com>
2025-02-05 22:58:51 +0900

    fix[import]: none use import 제거

--- core/network/src/main/java/com/hmh/hamyeonham/core/network/auth/interceptor/AuthInterceptor.kt
+++ core/network/src/main/java/com/hmh/hamyeonham/core/network/auth/interceptor/AuthInterceptor.kt
@@ -1,23 +1,9 @@
 package com.hmh.hamyeonham.core.network.auth.interceptor
 
-import android.content.Context
-import com.hmh.hamyeonham.common.navigation.NavigationProvider
-import com.hmh.hamyeonham.core.database.manger.DatabaseManager
-import com.hmh.hamyeonham.core.network.auth.api.RefreshService
 import com.hmh.hamyeonham.core.network.auth.authenticator.AuthenticatorUtil
-import com.hmh.hamyeonham.core.network.auth.datastore.network.HMHNetworkPreference
-import com.jakewharton.processphoenix.ProcessPhoenix
-import com.kakao.sdk.user.UserApiClient
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import okhttp3.Authenticator
 import okhttp3.Interceptor
-import okhttp3.Request
 import okhttp3.Response
-import okhttp3.Route
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
