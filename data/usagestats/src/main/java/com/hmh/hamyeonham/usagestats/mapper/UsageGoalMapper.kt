2816bfed7d4ea8425f88de57f7e21e74af8c0444
kangyuri1114 <yuri696@naver.com>
2025-08-18 23:28:41 +0900

    remove: usageGoalMapper 수정

--- data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/mapper/UsageGoalMapper.kt
+++ data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/mapper/UsageGoalMapper.kt
@@ -1,21 +1,7 @@
 package com.hmh.hamyeonham.usagestats.mapper
 
 import com.hmh.hamyeonham.core.database.model.UsageGoalEntity
-import com.hmh.hamyeonham.core.domain.usagegoal.model.ChallengeStatus
 import com.hmh.hamyeonham.core.domain.usagegoal.model.UsageGoal
-import com.hmh.hamyeonham.core.network.challenge.model.ChallengeResponse
-
-internal fun ChallengeResponse.toUsageGoalList(): UsageGoal {
-    val status =
-        if (todayIndex > -1) ChallengeStatus.fromString(statuses[todayIndex]) else ChallengeStatus.NONE
-    return UsageGoal(
-        status = status,
-        totalGoalTime = goalTime,
-        appGoals = apps.map { it.toApp() }
-    )
-}
-
-internal fun ChallengeResponse.AppGoal.toApp() = UsageGoal.App(appCode, goalTime)
 
 internal fun UsageGoalEntity.toUsageAppGoal() = UsageGoal.App(packageName, goalTime)
 
